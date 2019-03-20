21142e956a6e3d56d5f7b6d54d78eeac6106993b
UserWEB <joni_95@tut.by>
2019-03-20 09:35:38 +0300

    Add templates and view

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.all().count()
+    num_genres = Genre.objects.all().count()
+
+    return render(
+        request,
+        'catalog/index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_genres': num_genres},
+    )
