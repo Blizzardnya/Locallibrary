68b64cde1bc2ce82cce76ace26a6cd1c9e18c129
UserWEB <joni_95@tut.by>
2019-03-20 11:30:45 +0300

    Add templates and views

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+
 from . models import *
 
 # Create your views here.
@@ -18,3 +20,27 @@ def index(request):
                  'num_instances_available': num_instances_available, 'num_authors': num_authors,
                  'num_genres': num_genres},
     )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'catalog/book/book_list.html'
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'catalog/book/book_detail.html'
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'
+    template_name = 'catalog/author/author_list.html'
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = 'catalog/author/author_detail.html'
